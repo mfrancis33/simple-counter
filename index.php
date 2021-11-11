a0e22e28bb2ccd4b8f2a4a9a5f88a66165ff7b7d
M. Francis <58670911+mfrancis33@users.noreply.github.com>
2021-11-10 19:12:36 -0500

    Crap I pasted the wrong file

--- index.php
+++ index.php
@@ -1,21 +1,49 @@
-var app = require('http').createServer(handler);
-var io = require('socket.io').listen(app);
-var static = require('node-static'); // for serving files
-
-//Create a new server
-var fileServer = new static.Server('./');
-
-//The port to listen to
-app.listen(port);
-
-/**
- * Serves files from the server when opened in a browser
- * @param {IncomingMessage} request The request from the client
- * @param {ServerResponse} response The response to the client
- */
-function handler(request, response) {
-	request.addListener('end', function () {
-		//Return the correct file
-		fileServer.serve(request, response);
-	});
-}
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Counter</title>
+		<style>
+			body {
+				width: 100vw;
+				height: 100vh;
+				display: flex;
+				align-items: center;
+				justify-content: center;
+				background-color: monospace;
+				color: white;
+				font-family: sans-serif;
+				flex-direction: column;
+				margin: 0;
+				font-size: 10vmin;
+			}
+			button {
+				width: 20vmin;
+				height: 20vmin;
+				font-size: 10vmin;
+				font-weight: bold;
+				border: 0;
+				border-radius: 50%;
+			}
+			#u {
+				background-color: lime;
+			}
+			#d {
+				background-color: red;
+			}
+		</style>
+	</head>
+	<body>
+		<button id="u">&uarr;</button>
+		<h1>0</h1>
+		<button id="d">&darr;</button>
+		<script>
+			const u = document.getElementById("u");
+			const d = document.getElementById("d");
+			const h = document.querySelector("h1");
+			var count = 0;
+			u.onclick = function(){count++; h.innerText = count;};
+			d.onclick = function(){count--; h.innerText = count;};
+		</script>
+	</body>
+</html>
